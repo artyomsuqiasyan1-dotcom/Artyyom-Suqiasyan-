f62de7cbf97d2b126fe7c96e8963ac03b6d3e2a9
Student <artyom.suqiasyan1@tumo.org>
2026-05-19 19:53:53 +0400

    javascript review you

---
+++ src/practice.js
@@ -0,0 +1,24 @@
+const name="artyom"
+let count= 0
+console.log(name);
+console.log(count);
+
+let mron= "km"*0.621
+
+console.log(mron);
+
+const km1 =(km) => km * 0.621;
+console.log(km1(408))
+
+function parametr(name,craft) {
+    '${name}, is currently abard the, ${craft}'
+}
+console.log(parametr);
+
+const issPosition = {
+  latitude: 78.3543,
+  longitude: 9.6776
+};
+
+console.log(issPosition.latitude);
+console.log(issPosition.longitude);
